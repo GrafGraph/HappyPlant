be226db1b34e2f0d3f4167ad0236c0482da8fc8f
Anton Bespalov <56876475+kanton1998@users.noreply.github.com>
2020-11-14 11:52:31 +0100

    Spring added

---
+++ src/test/java/com/fherfurt/HappyPlant/HappyPlantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fherfurt.HappyPlant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyPlantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
