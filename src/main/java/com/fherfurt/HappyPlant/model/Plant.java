91aef19dea29a22e4f3939afe5dcfc4ed9a6c9f0
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2020-11-18 15:50:11 +0100

    created plant Class and added Thymeleaf dependency

---
+++ src/main/java/com/fherfurt/HappyPlant/model/Plant.java
@@ -0,0 +1,35 @@
+package com.fherfurt.HappyPlant.model;
+
+public class Plant {
+
+    private String name;
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Plant() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Plant(String name, String status) {
+        this.name = name;
+        this.status = status;
+    }
+
+    public String howAmI(){
+        return "My Name is " + this.name + " and i am " + this.status;
+    }
+}
