abd715945f539d54fd259a16d7a4bd9a4a5beb77
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2020-11-21 13:09:35 +0100

    Added greeting index and first plant

--- src/main/java/com/fherfurt/HappyPlant/config/WebAppController.java
+++ src/main/java/com/fherfurt/HappyPlant/config/WebAppController.java
@@ -1,12 +1,12 @@
 package com.fherfurt.HappyPlant.config;
 
 import com.fherfurt.HappyPlant.model.Plant;
-import jdk.nashorn.internal.objects.annotations.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -15,23 +15,25 @@ public class WebAppController {
     @Autowired
     public PlantRepository plantRepository;
 
-    @GetMapping(path = "/plants")
-    public @ResponseBody
-    Iterable<Plant> getAllPlants() {
-        // This returns a JSON or XML with the Plants
-        return plantRepository.findAll();
+    @GetMapping("/plants")
+    public String getAllPlants(Model model) {
+        Plant plant = new Plant("Willie", "Happy as a honeycake horse");
+        model.addAttribute(plant);
+//        Iterable<Plant> plants = plantRepository.findAll();   TODO: Read and Write Plant objects into DB
+        return "plants";
     }
 
-    @GetMapping(value="/plant/{plantId}")
-    public @ResponseBody
-    String getOnePlant(@PathVariable Integer  plantId, Model model) {
-        // This returns a JSON or XML with the one user
-       model.addAttribute("plantName", plantRepository.findById(plantId).get().getName());
-        return "plant";
-    }
+//    @GetMapping(value="/plant/{plantId}")
+//    public @ResponseBody
+//    String getOnePlant(@PathVariable Integer  plantId, Model model) {
+//        // This returns a JSON or XML with the one user
+//       model.addAttribute("plantName", plantRepository.findById(plantId).get().getName());
+//        return "plant";
+//    }
+
     @GetMapping("/")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(Model model) {
+        model.addAttribute("name", "Plant");
         return "greeting";
     }
 }
--- src/main/java/com/fherfurt/HappyPlant/model/Plant.java
+++ src/main/java/com/fherfurt/HappyPlant/model/Plant.java
@@ -1,20 +1,28 @@
 package com.fherfurt.HappyPlant.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Plant {
 
-    private Integer id;
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+
     private String name;
     private String status;
 
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
+    public Plant(String name, String status) {
+        this.name = name;
         this.status = status;
     }
 
+    // Constructor without Parameters for JPA.
     public Plant() {
+    }
+
+    public String howAmI(){
+        return "My Name is " + this.name + " and i am " + this.status;
     }
 
     public String getName() {
@@ -25,12 +33,19 @@ public class Plant {
         this.name = name;
     }
 
-    public Plant(String name, String status) {
-        this.name = name;
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
         this.status = status;
     }
 
-    public String howAmI(){
-        return "My Name is " + this.name + " and i am " + this.status;
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 }
