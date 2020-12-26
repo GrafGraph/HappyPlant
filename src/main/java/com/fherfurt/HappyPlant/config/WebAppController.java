e35ca49ed285545c0c3abcab6b2cc2e11667b143
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2020-12-26 20:43:28 +0100

    renamed greeting to index, deleted plants.html

--- src/main/java/com/fherfurt/HappyPlant/config/WebAppController.java
+++ src/main/java/com/fherfurt/HappyPlant/config/WebAppController.java
@@ -1,10 +1,6 @@
 package com.fherfurt.HappyPlant.config;
 
-import com.fherfurt.HappyPlant.helper.DataFaker;
-import com.fherfurt.HappyPlant.model.Plant;
-import com.fherfurt.HappyPlant.model.PlantStatus;
 import com.fherfurt.HappyPlant.model.SensorData;
-import com.fherfurt.HappyPlant.model.SensorDataEntry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,44 +10,22 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class WebAppController {
 
-    @Autowired
-    public PlantRepository plantRepository;
-
     @GetMapping("/")
-    public String greeting(Model model) {
-        //  = new DataFaker().createSensorDataHistoryString(DataFaker.DEFAULT_BACKTRACK_LENGTH,32);
+    public String index(Model model) {
         SensorData sensorData = new SensorData();
         String[] labels = sensorData.getTimestamps();
         String[] moistureValues = sensorData.getMoistureValues();
-        double wateringBorder = 32;     // TODO: Should be read from plant object
+        double wateringBorder = 32;     // Hint: Should be read from plant object in later versions of prototype
 
         model.addAttribute("size", sensorData.getEntries().size());
         model.addAttribute("labels", labels);
         model.addAttribute("moistureValues", moistureValues);
         model.addAttribute("wateringBorder", wateringBorder);
-        return "greeting";
+        return "index";
     }
-
-    @GetMapping("/plants")
-    public String getAllPlants(Model model) {
-        Plant plant = new Plant("Willie", PlantStatus.WATERED);
-        model.addAttribute(plant);
-//        Iterable<Plant> plants = plantRepository.findAll();   TODO: Read and Write Plant objects into DB
-        return "plants";
-    }
-
-//    @GetMapping(value="/plant/{plantId}")
-//    public @ResponseBody
-//    String getOnePlant(@PathVariable Integer  plantId, Model model) {
-//        // This returns a JSON or XML with the one user
-//       model.addAttribute("plantName", plantRepository.findById(plantId).get().getName());
-//        return "plant";
-//    }
 
     @GetMapping("/dataFaker")
     public String dataFaker(Model model) {
-        // Hardcoded wateringBorder of 32 for testing purposes. TODO: change to GET Parameter?
-        // Vector<SensorDataEntry> sensorData = new DataFaker().createSensorDataHistory(DataFaker.DEFAULT_BACKTRACK_LENGTH,32);
         SensorData sensorData = new SensorData();
         model.addAttribute("sensorData", sensorData.getEntries());
         return "dataFaker";
